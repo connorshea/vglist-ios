4ea6229b6dc582cc32d25e441a384da6a7a1c06d
Connor Shea <connor.james.shea@gmail.com>
2021-04-23 22:25:23 -0600

    Playing around with some basic code.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center) {
+            Text("vglist")
+                .font(.title2)
+                .fontWeight(.medium)
+                .padding(.all, 5)
+            Text("Track your entire video game library across every store and platform.")
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+                .fixedSize(horizontal: false, vertical: true)
+                .frame(width: 300)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
