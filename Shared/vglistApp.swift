1252695050a5d4060cf31ffd665b463842cef57b
Connor Shea <connor.james.shea@gmail.com>
2021-04-23 21:59:27 -0600

    Initial Commit

---
+++ Shared/vglistApp.swift
@@ -0,0 +1,17 @@
+//
+//  vglistApp.swift
+//  Shared
+//
+//  Created by Connor Shea on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct vglistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
